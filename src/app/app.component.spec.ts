55e8e9a14770ac4f1d499eed7c40e7e00fa77387
kishor96k <chavankishor813@gmail.com>
2024-02-02 11:54:58 +0530

    title changed for the main

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FoodOrder'`, () => {
+  it(`should have as title 'AngularProjects'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FoodOrder');
+    expect(app.title).toEqual('AngularProjects');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FoodOrder app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularProjects app is running!');
   });
 });
