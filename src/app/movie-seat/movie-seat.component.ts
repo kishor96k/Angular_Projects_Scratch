81e9c0fa58219caa098e7bafc36c9852fe8464d8
kishor96k <chavankishor813@gmail.com>
2024-02-11 09:28:03 +0530

    changes for the movieseatapp

---
+++ src/app/movie-seat/movie-seat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-seat',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-seat.component.html',
+  styleUrl: './movie-seat.component.css'
+})
+export class MovieSeatComponent {
+
+}
